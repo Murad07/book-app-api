28b8e77e74115220fd30023d22fb6644c65bf0ed
Murad Hossain <murad@tajtechltd.com>
2023-08-23 19:30:39 +0600

    convert price and rating to numbers

--- api/get_books.php
+++ api/get_books.php
@@ -6,6 +6,10 @@ $result = mysqli_query($connection, $query);
 
 $books = [];
 while ($row = mysqli_fetch_assoc($result)) {
+    // Convert price and rating to numbers
+    $row['price'] = floatval($row['price']);
+    $row['rating'] = floatval($row['rating']);
+    
     $books[] = $row;
 }
 
