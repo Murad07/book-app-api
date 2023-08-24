a713001f323c7424979ac1ddb22ad5d12e565ded
Murad Hossain <murad@tajtechltd.com>
2023-08-24 13:17:01 +0600

    modify add and update api

--- api/add_book.php
+++ api/add_book.php
@@ -4,15 +4,17 @@ include 'connect.php';
 // Retrieve JSON data from the request body
 $data = json_decode(file_get_contents("php://input"), true);
 
-$name = $data['name'];
-$author = $data['author'];
-$thumbnail = $data['thumbnail'];
-$price = $data['price'];
-$rating = $data['rating'];
-$featured = $data['featured'];
+if ($data) {
+    $name = $data['name'];
+    $author = $data['author'];
+    $thumbnail = $data['thumbnail'];
+    $price = $data['price'];
+    $rating = $data['rating'];
+    $featured = $data['featured'];
 
-$query = "INSERT INTO books (name, author, thumbnail, price, rating, featured) VALUES ('$name', '$author', '$thumbnail', '$price', '$rating', '$featured')";
-$result = mysqli_query($connection, $query);
+    $query = "INSERT INTO books (name, author, thumbnail, price, rating, featured) VALUES ('$name', '$author', '$thumbnail', '$price', '$rating', '$featured')";
+    $result = mysqli_query($connection, $query);
+}
 
 if ($result) {
     echo json_encode(['message' => 'Book added successfully']);
--- api/update_book.php
+++ api/update_book.php
@@ -4,16 +4,18 @@ include 'connect.php';
 // Retrieve JSON data from the request body
 $data = json_decode(file_get_contents("php://input"), true);
 
-$id = $data['id'];
-$name = $data['name'];
-$author = $data['author'];
-$thumbnail = $data['thumbnail'];
-$price = $data['price'];
-$rating = $data['rating'];
-$featured = $data['featured'];
+if ($data) {
+    $id = $data['id'];
+    $name = $data['name'];
+    $author = $data['author'];
+    $thumbnail = $data['thumbnail'];
+    $price = $data['price'];
+    $rating = $data['rating'];
+    $featured = $data['featured'];
 
-$query = "UPDATE books SET name = '$name', author = '$author', thumbnail = '$thumbnail', price = '$price', rating = '$rating', featured = '$featured' WHERE id = $id";
-$result = mysqli_query($connection, $query);
+    $query = "UPDATE books SET name = '$name', author = '$author', thumbnail = '$thumbnail', price = '$price', rating = '$rating', featured = '$featured' WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+}
 
 if ($result) {
     echo json_encode(['message' => 'Book updated successfully']);
